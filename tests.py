77ca71a447ecf31191473840979e4890193a28be
Fernanda Diomede <fcdiomede@gmail.com>
2020-07-27 14:58:01 -0500

    Added unit test for rsvping

--- tests.py
+++ tests.py
@@ -23,6 +23,8 @@ class PartyTests(unittest.TestCase):
         """Do users who haven't RSVPed see the correct view?"""
 
         result = self.client.get('/')
+
+         #check that we are prompted to RSVP and don't see party details without it.
         self.assertIn(b'Please RSVP', result.data)
         self.assertNotIn(b'Party Details', result.data)
 
@@ -34,8 +36,9 @@ class PartyTests(unittest.TestCase):
         result = self.client.post('/rsvp', data=rsvp_info,
                                   follow_redirects=True)
 
-        # FIXME: check that once we log in we see party details--but not the form!
-        print('FIXME')
+        #check that once we log in we see party details--but not the form!
+        self.assertIn(b'Party Details', result.data)
+        self.assertNotIn(b'Please RVSP', result.data)
 
     def test_rsvp_mel(self):
         """Can we keep Mel out?"""
